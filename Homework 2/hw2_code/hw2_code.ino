11733cb91f5a328cd619745dae6a82e50c6164cd
Alexandru Mihai <115073416+AlexMihai1126@users.noreply.github.com>
2023-10-30 12:43:29 +0200

    Update hw2_code.ino

--- Homework 2/hw2_code/hw2_code.ino
+++ Homework 2/hw2_code/hw2_code.ino
@@ -9,18 +9,22 @@
 #define secondFloorBtn 12
 #define thirdFloorBtn 13
 #define doorCloseTimer 1500
-#define moveTime 4500
+#define moveTime 5000
 #define waitTime 2000
 #define debounceTime 150
 
 int currentFloor = 0;
 bool elevatorMoving = false;
-bool firstFloorBtnPressed=false;
-bool secondFloorBtnPressed=false;
-bool thirdFloorBtnPressed=false;
+bool firstFloorBtnLastReading=LOW;
+bool secondFloorBtnLastReading=LOW;
+bool thirdFloorBtnLastReading=LOW;
+bool firstFloorBtnState=LOW;
+bool secondFloorBtnState=LOW;
+bool thirdFloorBtnState=LOW;
 bool elevatorLEDCurrentState = LOW;
 unsigned long prevMillis=0;
 const int elevatorLEDBlinkInterval=250;
+unsigned long lastDebounceTime = 0;
 SimpleStack<int> floorStack(10);
 
 void floorIndicators(int floor) {
@@ -98,5 +102,51 @@ void setup() {
 }
 
 void loop() {
-  elevatorState(2, true);
+  bool firstFloorBtnReading = digitalRead(firstFloorBtn);
+  bool secondFloorBtnReading = digitalRead(secondFloorBtn);
+  bool thirdFloorBtnReading digitalRead(thirdFloorBtn);
+
+  if (firstFloorBtnReading != firstFloorBtnLastReading) {
+    lastDebounceTime = millis();
+  }
+  if ((millis() - lastDebounceTime) > debounceTime) {
+    if (firstFloorBtnReading != firstFloorBtnState) {
+      firstFloorBtnState = firstFloorBtnReading;
+      if (firstFloorBtnReading == HIGH) {
+        //cod pt ce sa se intample cand butonul este HIGH
+      }
+    }
+  }
+  firstFloorBtnLastReading = firstFloorBtnReading;
+ //pt buton 1
+
+  if (secondFloorBtnReading != secondFloorBtnLastReading) {
+    lastDebounceTime = millis();
+  }
+  if ((millis() - lastDebounceTime) > debounceTime) {
+    if (secondFloorBtnReading != secondFloorBtnState) {
+      secondFloorBtnState = secondFloorBtnReading;
+      if (secondFloorBtnReading == HIGH) {
+        //cod pt ce sa se intample cand butonul este HIGH
+      }
+    }
+  }
+  secondFloorBtnLastReading = secondFloorBtnReading;
+//pt buton 2
+
+
+  if (thirdFloorBtnReading != thirdFloorBtnLastReading) {
+    lastDebounceTime = millis();
+  }
+  if ((millis() - lastDebounceTime) > debounceTime) {
+    if (thirdFloorBtnReading != thirdFloorBtnState) {
+      thirdFloorBtnState = thirdFloorBtnReading;
+      if (thirdFloorBtnReading == HIGH) {
+        //cod pt ce sa se intample cand butonul este HIGH
+      }
+    }
+  }
+  thirdFloorBtnLastReading = thirdFloorBtnReading;
+
+  //pt buton 3
 }
